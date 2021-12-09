48ceb0cd2d91d75eca789f205b515da397c015c8
florentCourtin <92575172+florentCourtin@users.noreply.github.com>
2021-12-09 22:05:46 +0100

    Update Meta.java

--- metadonnees/Meta.java
+++ metadonnees/Meta.java
@@ -12,13 +12,13 @@ public class Meta {
 	private String typeFile;
 	private File f;
 	private String path;
-	private ArrayList<MetaImage> imageMetaArray;
+	private ArrayList<MetaImage> metaImageArray;
 	
 	public Meta(String typeFile, String path) throws IsNotDirectoryException, IsNotFileException {
 		this.typeFile = typeFile;
 		this.path = path;
 		this.f = new File(path);
-		this.imageMetaArray = new ArrayList<MetaImage>();
+		this.metaImageArray = new ArrayList<MetaImage>();
 		if (typeFile == "-d" && !f.isDirectory()) {
 			throw new IsNotDirectoryException(f);
 		} else if (typeFile == "-f" && !f.isFile()) {
@@ -35,7 +35,7 @@ public class Meta {
 				String mimeType = new Tika().detect(files[i].getPath());
 				if ((mimeType.equals("image/jpeg")) || (mimeType.equals("image/png"))) {
 					MetaImage mi = new MetaImage(files[i].getPath());
-					imageMetaArray.add(mi);
+					metaImageArray.add(mi);
 					mi.extractMetaImage();
 				}
 			}
@@ -46,7 +46,7 @@ public class Meta {
 		if (typeFile == "-f") {
 			MetaImage m = new MetaImage(path);
 			m.extractMetaImage();
-			imageMetaArray.add(m);
+			metaImageArray.add(m);
 		}
 		if (typeFile == "-d") {
 			File[] files = f.listFiles();
@@ -56,8 +56,8 @@ public class Meta {
 	
 	public String toString() {
 		String result = "Le repertoire contient:\n\n";
-		for (int i=0; i<imageMetaArray.size(); i++) {
-			result += imageMetaArray.get(i).toString() + "\n";
+		for (int i=0; i<metaImageArray.size(); i++) {
+			result += metaImageArray.get(i).toString() + "\n";
 		}
 		return result;
 	}

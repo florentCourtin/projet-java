89ea7e8a235099a636c976564fc62afda471874f
florentCourtin <92575172+florentCourtin@users.noreply.github.com>
2021-12-23 00:33:50 +0100

    Update IsNotDirectoryException.java

--- metadonnees/IsNotDirectoryException.java
+++ metadonnees/IsNotDirectoryException.java
@@ -6,6 +6,6 @@ public class IsNotDirectoryException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public IsNotDirectoryException(File f) { 
-		super(f.getName() + " n'est pas un repertoire.\nPour extraire les metadonnees d'un fichier, utilisez l'option -f.");
+		super(f.getName() + " n'est pas un repertoire.\nPour plus de precision, utilisez l'option -h.");
 	}
 }

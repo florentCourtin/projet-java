fb7a792fc77562a520c135eb3901fe622fbf6f09
florentCourtin <92575172+florentCourtin@users.noreply.github.com>
2021-12-23 00:36:00 +0100

    Update IsNotFileException.java

--- metadonnees/IsNotFileException.java
+++ metadonnees/IsNotFileException.java
@@ -6,6 +6,6 @@ public class IsNotFileException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public IsNotFileException(File f) {
-		super(f.getName() + " n'est pas un fichier.\nPour extraire l'ensemble des metadonnees d'un repertoire, utilisez l'option -d.");
+		super(f.getName() + " n'est pas un fichier.\nPour plus de precision, utilisez l'option -h.");
 	}
 }

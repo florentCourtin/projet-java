cd95ec86460b6b8b73c1b91e173cc25cbf60e402
florentCourtin <92575172+florentCourtin@users.noreply.github.com>
2021-12-23 01:25:11 +0100

    Update Meta.java

--- metadonnees/Meta.java
+++ metadonnees/Meta.java
@@ -23,7 +23,6 @@ public class Meta {
 			throw new IsNotDirectoryException(f);
 		} else if (typeFile == "-f" && !f.isFile()) {
 			throw new IsNotFileException(f);
-			//+else if pour option -h ?
 		}
 	}
 	
@@ -54,11 +53,61 @@ public class Meta {
 		}
 	}
 	
+	public String getTypeFile() {
+		return typeFile;
+	}
+
+	public ArrayList<MetaImage> getMetaImageArray() {
+		return metaImageArray;
+	}
+
 	public String toString() {
-		String result = "Le repertoire contient:\n\n";
+		String result = "Le repertoire " + f.getName() + " contient:\n\n";
 		for (int i=0; i<metaImageArray.size(); i++) {
 			result += metaImageArray.get(i).toString() + "\n";
 		}
 		return result;
+	}
+	
+	public static void main(String[] args) {
+		Meta m;
+		
+		if (args[0].equals("-d")) {
+			try {
+				m = new Meta("-d", args[1]);
+				m.extractMeta();
+				System.out.println(m);
+			} catch (IsNotDirectoryException e) {
+				e.printStackTrace();
+			} catch (IsNotFileException e) {
+				e.printStackTrace();
+			} catch (ImageProcessingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else if (args[0].equals("-f")) {
+			try {
+				m = new Meta("-f", args[1]);
+				m.extractMeta();
+				System.out.println(m);
+			} catch (IsNotDirectoryException e) {
+				e.printStackTrace();
+			} catch (IsNotFileException e) {
+				e.printStackTrace();
+			} catch (ImageProcessingException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+		} else if (args[0].equals("-h") || (args[0].isEmpty())) {
+			System.out.println("Vous avez ouvert l'option d'aide -h pour la classe Meta");
+			System.out.println("Cette classe permet d'extraire les metadonnees d'un fichier image, ou d'un repertoire");
+			System.out.println("Voici les arguments possibles: ");
+			System.out.println("    -d, chemin        -d pour preciser que c'est un repertoire, chemin pour retrouver le repertoire en question");
+			System.out.println("    -f, chemin        -f pour preciser que c'est un fichier, chemin pour retrouver le fichier en question");
+		} else {
+			System.out.println("Erreur: pour afficher l'aide, utilisez l'option -h.");
+		}
 	}
 }
